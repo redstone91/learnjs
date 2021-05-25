b5e42d2f1d153421ec6f0cccfc6f896dba85590e
Tyomik <macbook@Tyomiks-MacBook-Pro.local>
2021-05-25 13:23:07 +0300

    lesson1

---
+++ js/js1.js
@@ -0,0 +1,17 @@
+let string = "строка"
+console.log(string);
+
+let number = 10
+alert(number);
+
+
+
+
+function Profile(num) {
+    alert(num * 25)
+}
+
+Profile(10)
+
+
+//https://github.com/redstone91/learnjs.git
---
+++ js/script.js
@@ -0,0 +1,37 @@
+// названиеФункция()
+function Profile(name) {
+    console.log("Функция вызвалась");
+    alert("Привет! " + name)
+}
+
+// Вывод в консоль
+// console.log("JS");
+
+// Переменные
+// строкые: "текст" 
+
+// числовые: 
+// целые числа: 45
+// Не целые числа: 37.8
+
+// Масивые: [10, "Ivan", 10.8, 10.5];
+
+// camelCase
+let countDialog = 10; // Её можем менять в коде
+const pi = 3.1415;// Её вкоде менять не можем
+
+
+// countDialog = countDialog + 5;
+console.log(countDialog);
+
+//alert()
+let Na = "Артём"
+let Sa = 'Саша'
+Profile(Na) // Вызов функции
+Profile(Sa)
+
+
+
+
+
+
