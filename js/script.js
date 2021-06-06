dbb110bad4831e039376f6ce3a96bb8c76e2f700
Tyomik <macbook@Tyomiks-MacBook-Pro.local>
2021-06-06 13:54:05 +0300

    Lesson 5.1

--- js/script.js
+++ js/script.js
@@ -1,73 +1,27 @@
-// По id ("#text-1")
-// По тегу ("div")
-// По классу (".content")
+// 
 
+let bodyElement = document.querySelector('.body');
+bodyElement.style.background = "lime";
 
-// Возьмика и собери мне массив всех элементов, у которых есть класс/id/теги одинаквоые
-// querySelectorAll() - всегда возвращает(собирает) список(массив) с элементами
+let bodyTextElements = document.querySelectorAll('.body__text');
 
-// querySelector() - возвращает 1 элемент (самый первый, который найдёт, неважно класс или id)
-// let ex = querySelector() // ex = "element"
-// let ex = querySelectorAll() // ex = ["element"]
+// // console.log(bodyTextElements.length);
+// bodyTextElements[0].style.color = "blue";
+// bodyTextElements[1].style.color = "blue";
+// bodyTextElements[2].style.color = "blue";
+// bodyTextElements[3].style.color = "blue";
 
+// Вывести в консоль 5 раз какой-то текст
+for (index = 0; index < bodyTextElements.length; index++) {
+    if (index == 2) {
+        bodyTextElements[index].style.color = "red";
+    } else {
+        bodyTextElements[index].style.color = "blue";
+    }
 
-
-// cont.style.color = "red";
-
-// Массив
-let array = ["el1", "el2", "el3"]
-console.log(array[0])
-
-
-// Задача дать стиль нижнему блоку body__text
-// let BodyText = document.querySelectorAll(".body__text")
-// console.log(BodyText[1])
-
-// BodyText[1].style.backgroundColor = "tomato";
-
-
-// let cont = document.querySelectorAll(".content")
-// console.log(cont[2])
-
-
-// let Text = document.querySelector("#text-3")
-// console.log(Text);
-
-// Text.style.fontSize = "30px";
+}
 
 
 
-// let body = document.querySelector(".body");
-// console.log(body);
-// body.style.backgroundColor = "green";
 
 
-// let BodyText = document.querySelectorAll(".body__text")
-// console.log(BodyText)
-
-// BodyText[0].style.color = "blue";
-// BodyText[1].style.color = "blue";
-
-let num = Number(prompt()); // num = "7"
-let num2 = Number(prompt()); // num = "99"
-let num3 = Number(prompt()); // num = "21"
-
-// 1 2 3 4 5 6 7 8 9 0
-
-// let num = 7
-// let num2 = 99
-// let num3 = 21
-
-
-// if ((num > num3 && num < num2) || (num < num3 && num > num2)) {
-//     console.log(num)
-// }
-// else if (num2 > num && num2 < num3 || num2 < num && num2 > num3) {
-//     console.log(num2)
-// }
-// else if (num3 > num2 && num3 < num || num3 < num2 && num3 > num) {
-//     console.log(num3)
-// }
-// else {
-//     console.log("одинаковые")
-// }
