3e03e243a7f4906480c5d949879ac01555f85f22
Tyomik <macbook@Tyomiks-MacBook-Pro.local>
2021-06-15 13:43:52 +0300

    Lesson 9

--- js/script.js
+++ js/script.js
@@ -1,88 +1,34 @@
-// HomeWork
-// let contentBottomElement = document.querySelector('.content-bottom');
-// console.log(contentBottomElement);
-// let textElement = contentBottomElement.querySelector('.text');
-
-// for (let index = 1; index < 8; index++) {
-//     console.log("BubleGum");
-
-// }
-
-
-// for (let index = 0; index < 10; index++) {
-//     console.log("Relax");
-//     if (index == 2 || index == 6) {
-//         console.log("Boom");
-//     }
-
-// }
-
-// let arr = [25, 30.5, 40, 50, "ivan"];
-// for (let index = 0; index < 4; index++) {
-//     console.log(arr[index]);
-
-// }
-
-
-// Обратиться к списку с классом content-bottom__list 
-// Вывести в терминал все элменты этого списка
-// Покрасить все элементы в синий цвет, только 1 и последний элемент - в красный
-
-// let contentBottomListElement = document.querySelector('.content-bottom__list');
-// let liElements = contentBottomListElement.querySelectorAll('li');
-// for (let index = 0; index < liElements.length; index++) {
-//     console.log(liElements[index]);
-//     if (index == 0 || index == liElements.length - 1) {
-//         liElements[index].style.color = "red";
-//     } else {
-//         liElements[index].style.color = "blue";
-//     }
-// }
 // let contentFormElement = document.querySelector('.content__form');
+// let inputElements = contentFormElement.querySelectorAll('input');
 
-
-// let btn1Element = contentFormElement.querySelector('#btn-1');
-// console.log(btn1Element);
-
-// let inputTextElement = contentFormElement.querySelector('#input-text');
-// console.log(inputTextElement);
-
-
-// // Если текстовое поле пустое, то вывести alert("введите текст")
-// function btnclk() {
-//     if (inputTextElement.value == "") {
-//         alert("text")
-//     } else {
-//         alert("Всё ок");
+// let checkBoxElement;
+// // console.log(inputElements[1].type);
+// for (let index = 0; index < inputElements.length; index++) {
+//     if (inputElements[index].type == "checkbox") {
+//         console.log(index);
+//         // inputElements[index].checked = true;
+//         checkBoxElement = inputElements[index];
 //     }
 // }
 
 
+// Посчитать количество нажатых чекбоксов
+let footerElement = document.querySelector('.footer');
+let inputElements = footerElement.querySelectorAll('input');
 
+let countCheckedCheckBox = 0;
 
-// for (let index = 0; index < 12; index++) {
-//     console.log("Random Text");
-// }
-
-// 0 1 2 3 4 // 0 1 2 3 4 5
-// for (let index = 0; index < 5; index++) {
-//     console.log("Last" + index);
-//     if (index == 0 || index == 4)
-//         console.log("hit");
-
-// }
-
-// let contentTopElement = document.querySelector('.content-top');
-// let liElements = contentTopElement.querySelectorAll('li');
-// for (let index = 0; index < liElements.length; index++) {
-//     console.log(liElements[index]);
-
-// }
-
-// let buttonElements = document.querySelectorAll('button');
-// for (let index = 0; index < buttonElements.length; index++) {
-//     console.log(buttonElements[index]);
-//     buttonElements[index].style.fontSize = "40px";
-
+for (let index = 0; index < inputElements.length; index++) {
+    if (inputElements[index].type == "checkbox" && inputElements[index].checked == true) {
+        countCheckedCheckBox++;
+    }
 }
+
+console.log(countCheckedCheckBox);
+
+
+
+
+
+
 
